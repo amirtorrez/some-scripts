f4ff2c951a4c71c051593e5ad8aa734a72eabc59
Amir Torres <johnamtorres@gmail.com>
2015-03-07 12:04:38 -0600

    Update winex64.sh

--- Wine/winex64.sh
+++ Wine/winex64.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+## Deleting Wine files in home folder
 cd ~
 rm -rf .wine
 rm -f .config/menus/applications-merged/wine*
@@ -7,4 +8,5 @@ rm -rf .local/share/applications/wine
 rm -f .local/share/desktop-directories/wine*
 rm -f .local/share/icons/????_*.xpm 
 
+## Exporting x32 prefix
 WINEARCH=win32 WINEPREFIX=~/.wine winecfg
