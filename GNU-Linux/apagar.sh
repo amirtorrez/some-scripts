7e0b9a5c73d5555beaec20645bdb84216cd7333a
Amir Torrez <johnamtorres@gmail.com>
2016-05-09 00:32:44 -0500

    Create apagar.sh

---
+++ GNU-Linux/apagar.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+clear
+echo " =========================================================================="
+echo " ==                                                                      =="
+echo " ==             Bienvenido al Sistema de Apagado Automático              =="
+echo " ==                        Creado por Amir Torrez                        =="
+echo " ==                                                                      =="
+echo " =========================================================================="
+echo "\n MENU"
+echo "   1 -> Determinar minutos"
+echo "   2 -> Fijar hora"
+echo "   3 -> Salir\n"
+read -p " ?: " OPCION
+case $OPCION in
+	1) echo -n "\nIngresa el numero de minutos, tras cual el PC se apagará: "
+	   read minutos
+	   sudo shutdown -h $minutos;;
+	2) echo -n "\nIngresa la hora de apagado: "
+	   read hora
+	   sudo shutdown -h $hora;;
+	3) exit;;
+	*) echo "\nSe ingresó una opción que no está en el menú"
+	   exit 1;;
+esac
+exit 0
