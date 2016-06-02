6eb6e3c43b3a9f646503ba4590946b975adaad02
Amir Torrez <johnamtorres@gmail.com>
2016-06-02 00:42:57 -0500

    Update apagar.sh

--- GNU-Linux/apagar.sh
+++ GNU-Linux/apagar.sh
@@ -9,7 +9,8 @@ echo " =========================================================================
 echo "\n MENU"
 echo "   1 -> Determinar minutos"
 echo "   2 -> Fijar hora"
-echo "   3 -> Salir\n"
+echo "   3 -> Cancelar apagado"
+echo "   4 -> Salir\n"
 read -p " ?: " OPCION
 case $OPCION in
 	1) echo -n "\nIngresa el numero de minutos, tras cual el PC se apagará: "
@@ -18,7 +19,10 @@ case $OPCION in
 	2) echo -n "\nIngresa la hora de apagado: "
 	   read hora
 	   sudo shutdown -h $hora;;
-	3) exit;;
+	3) echo -n "\nCancelando apagado...\n"
+	   sudo shutdown -c
+	   echo -n "\n¡Apagado automático cancelado!\n\n";;
+	4) exit;;
 	*) echo "\nSe ingresó una opción que no está en el menú"
 	   exit 1;;
 esac
