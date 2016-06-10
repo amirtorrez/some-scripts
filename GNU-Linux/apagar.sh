73fa73c778c143941ef2d98da919f29a91617a0c
Amir Torrez <johnamtorres@gmail.com>
2016-06-09 20:31:08 -0500

    Update apagar.sh

--- GNU-Linux/apagar.sh
+++ GNU-Linux/apagar.sh
@@ -1,29 +1,32 @@
 #!/bin/sh
 clear
-echo " =========================================================================="
-echo " ==                                                                      =="
-echo " ==             Bienvenido al Sistema de Apagado Automático              =="
-echo " ==                        Creado por Amir Torrez                        =="
-echo " ==                                                                      =="
-echo " =========================================================================="
-echo "\n MENU"
-echo "   1 -> Determinar minutos"
-echo "   2 -> Fijar hora"
-echo "   3 -> Cancelar apagado"
-echo "   4 -> Salir\n"
-read -p " ?: " OPCION
-case $OPCION in
-	1) echo -n "\nIngresa el numero de minutos, tras cual el PC se apagará: "
-	   read minutos
-	   sudo shutdown -h $minutos;;
-	2) echo -n "\nIngresa la hora de apagado: "
-	   read hora
-	   sudo shutdown -h $hora;;
-	3) echo -n "\nCancelando apagado...\n"
-	   sudo shutdown -c
-	   echo -n "\n¡Apagado automático cancelado!\n\n";;
-	4) exit;;
-	*) echo "\nSe ingresó una opción que no está en el menú"
-	   exit 1;;
-esac
-exit 0
+until [ $OPCION  = "" ];
+do
+	echo " =========================================================================="
+	echo " ==                                                                      =="
+	echo " ==             Bienvenido al Sistema de Apagado Automático              =="
+	echo " ==                        Creado por Amir Torrez                        =="
+	echo " ==                                                                      =="
+	echo " =========================================================================="
+	echo "\n MENU"
+	echo "   1 -> Determinar tiempo"
+	echo "   2 -> Cancelar apagado"
+	echo "   3 -> Salir\n"
+
+	read -p " ?: " OPCION
+
+	if [ "$OPCION" = "1" ]; then
+		echo -n "\n Ingresa el numero de minutos, tras cual el PC se apagará,\n ó introduce el tiempo con el formato 00:00:00\n\n"
+		read -p " ?: " tiempo
+		sudo shutdown -h $tiempo
+	elif [ "$OPCION" = "2" ]; then
+		echo -n "\nCancelando apagado...\n"
+		sudo shutdown -c
+		echo -n "\n¡Apagado automático cancelado!\n\n"
+	elif [ "$OPCION" = "3" ]; then
+		echo -n "\n"
+		exit
+	else
+		echo "\nSe ingresó una opción que no está en el menú\n"
+	fi
+done
